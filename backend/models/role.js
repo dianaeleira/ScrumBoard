6f3e061fbdd1a166adab584ffd40a86971e1d075
Diana Triana <dianaeleira@servinformacion.com>
2021-08-09 14:42:42 -0500

    Development of logic of the services api rest POST and GET

---
+++ backend/models/role.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const roleSchema = new mongoose.Schema({
+    name: String,
+    description: String,
+    date: {type: Date, default:Date.now},
+    dbStatus: Boolean,
+})
+
+const role = mongoose.model("role", roleSchema);
+module.exports=role;
