9033df33ee7273f037f8c6d1191228724fa4cc7e
Diana Eleira Triana Suarez <dianaeleira@gmail.com>
2021-08-11 12:04:13 -0500

    Development of the Post and Get method for User, implementing bcrypt and JWToken.

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,24 +1,28 @@
-const mongoose = require("mongoose");
-const jwt = require("jsonwebtoken");
-const moment = require("moment");
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const moment = require('moment');
 
 const userSchema = new mongoose.Schema({
     name: String,
     email: String,
     password: String,
-    roleId: { type: mongoose.Schema.ObjectID, ref: "role" },
-    date: { type: Date, default: Date.now },
+    roleId: { type: mongoose.Schema.Types.ObjectId, ref: "role" },
+    date: { type: Date, default: Date.now() },
     dbStatus: Boolean,
-})
-userSchema.methods.generateJWT = function(){
-    return jwt.sign({
-        _id: this._id,
-        name: this.name,
-        iat: moment().unix(),
-    },
-    process.env.SECRET_KEY_JWT
-    )
+});
+
+userSchema.methods.generateJWT = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            name: this.name,
+            iat: moment().unix(),
+        },
+        process.env.SECRET_KEY_JWT
+    );
 }
 
 const user = mongoose.model("user", userSchema);
+
 module.exports = user;
+
